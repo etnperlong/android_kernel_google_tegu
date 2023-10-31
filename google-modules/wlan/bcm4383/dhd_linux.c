19139899146a94418749487e8de448ba7087192b
Sultan Alsawaf <sultan@kerneltoast.com>
2023-10-30 22:53:04 -0700

    bcmdhd: Disable as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- google-modules/wlan/bcm4383/dhd_linux.c
+++ google-modules/wlan/bcm4383/dhd_linux.c
@@ -6473,6 +6473,7 @@ dhd_force_collect_socram_during_wifi_onoff(dhd_pub_t *dhdp)
 #endif /* OEM_ANDROID */
 }
 
+#ifdef SHOW_LOGTRACE
 static void
 dhd_free_event_data_fmts_buf(dhd_info_t *dhd)
 {
@@ -6509,6 +6510,7 @@ dhd_free_event_data_fmts_buf(dhd_info_t *dhd)
 #endif /* COEX_CPU */
 
 }
+#endif /* SHOW_LOGTRACE */
 
 int
 dhd_stop(struct net_device *net)
@@ -6882,6 +6884,7 @@ dhd_force_collect_init_fail_dumps(dhd_pub_t *dhdp)
 #endif /* DEBUG_DNGL_INIT_FAIL */
 #endif /* CUSTOMER_HW4_DEBUG */
 
+#ifdef DHD_FW_COREDUMP
 	/* for android force collect socram for FW init failures
 	 * by putting bus state to LOAD
 	 */
@@ -6891,7 +6894,6 @@ dhd_force_collect_init_fail_dumps(dhd_pub_t *dhdp)
 	if (dhdp->busstate == DHD_BUS_DOWN) {
 		dhdp->busstate = DHD_BUS_LOAD;
 	}
-#ifdef DHD_FW_COREDUMP
 	/* save core dump or write to a file */
 	if (dhdp->memdump_enabled && (dhdp->busstate != DHD_BUS_DOWN)) {
 #ifdef DHD_SDTC_ETB_DUMP
@@ -9778,6 +9780,7 @@ dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
 	}
 #endif /* EWP_EDL */
 
+#ifdef DHD_FW_COREDUMP
 	/* alloc memory for socram during init itself, newer chips
 	 * require 4M and this requires vmalloc which will fail
 	 * if called from a non sleepable context
@@ -9786,6 +9789,7 @@ dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
 		DHD_ERROR(("%s: Failed to alloc memdump memory !\n", __FUNCTION__));
 		goto fail;
 	}
+#endif
 
 	/* Allocate core dump size after each section len is set up
 	 * coredump consist of hdr, socram, sssr, sdtc and coex
